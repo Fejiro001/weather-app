5238e619ebe546ca2f069cf088e161b220bb7611
Fejiro001 <aberefejiro@gmail.com>
2025-09-21 14:21:20 +0100

    Add error page component and integrate error handling in app layout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,16 +1,26 @@
 import { Header } from "../components/basic";
+import ErrorPage from "../pages/ErrorPage";
 import HomePage from "../pages/HomePage";
+import useWeatherStore from "../weatherStore";
 
 const AppLayout = () => {
+  const isError = useWeatherStore((state) => state.isError);
+
   return (
     <div className="layout">
       <Header />
 
-      <h1 className="text-preset-2 text-center text-balance px-5">
-        How's the sky looking today?
-      </h1>
+      {isError ? (
+        <ErrorPage />
+      ) : (
+        <>
+          <h1 className="text-preset-2 text-center text-balance px-5">
+            How's the sky looking today?
+          </h1>
 
-      <HomePage />
+          <HomePage />
+        </>
+      )}
     </div>
   );
 };
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,29 @@
+import errorIcon from "../assets/images/icon-error.svg";
+import retryIcon from "../assets/images/icon-retry.svg";
+
+const ErrorPage = () => {
+  return (
+    <main className="space-y-8 xl:space-y-12 w-full max-w-7xl mt-[1.125rem]">
+      <section className="flex flex-col items-center text-center gap-6">
+        <img
+          src={errorIcon}
+          alt="Error icon"
+          className="mx-auto h-auto w-[2.625rem]"
+        />
+        <h1 className="text-4xl md:text-[3.25rem] text-preset-2">
+          Something went wrong
+        </h1>
+        <p className="text-lg md:text-xl text-preset-5 font-medium text-(--neutral-200)">
+          We couldn't connect to the server (API error). Please try again in a
+          few moments.
+        </p>
+        <button className="bg-(--neutral-800) hover:bg-(--neutral-700) px-4 py-3.5 flex gap-2.5 rounded-lg">
+          <img src={retryIcon} />
+          <span className="text-preset-7">Retry</span>
+        </button>
+      </section>
+    </main>
+  );
+};
+
+export default ErrorPage;
